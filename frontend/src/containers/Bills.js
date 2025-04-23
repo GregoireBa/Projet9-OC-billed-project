99314008b2a0e28d4793a20575d4772d881cbf27
GregoireBa <gregoire-bar@hotmail.fr>
2025-04-23 14:37:21 +0200

    Fix: sort bills and make test pass (fixes #1)

--- frontend/src/containers/Bills.js
+++ frontend/src/containers/Bills.js
@@ -41,24 +41,30 @@ export default class {
         .bills()
         .list()
         .then((snapshot) => {
-          const bills = snapshot.map((doc) => {
-            try {
-              return {
-                ...doc,
-                date: formatDate(doc.date),
-                status: formatStatus(doc.status),
-              };
-            } catch (e) {
-              // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-              // log the error and return unformatted date in that case
-              console.log(e, "for", doc);
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status),
-              };
-            }
-          });
+          const bills = snapshot
+            .map((doc) => {
+              try {
+                return {
+                  ...doc,
+                  date: formatDate(doc.date),
+                  rawDate: doc.date,
+                  status: formatStatus(doc.status),
+                };
+              } catch (e) {
+                // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+                // log the error and return unformatted date in that case
+                console.log(e, "for", doc);
+                return {
+                  ...doc,
+                  date: doc.date,
+                  rawDate: doc.date,
+                  status: formatStatus(doc.status),
+                };
+              }
+            })
+            // Le tri des facture est fait ici
+            .sort((a, b) => new Date(b.rawDate) - new Date(a.rawDate));
+
           console.log("length", bills.length);
           return bills;
         });
